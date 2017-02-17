ec630e0f4fd86bfc3d7f75f9695545badcc193e0
Antonio Martins Miranda <amartmiranda@gmail.com>
2017-02-17 17:41:17 -0200

    Indentando o código no arquivo cria.blade.php e o corrigindo o erro do 'TokenMismatchException in VerifyCsrfToken.php'.

--- resources/views/editora/cria.blade.php
+++ resources/views/editora/cria.blade.php
@@ -6,40 +6,41 @@
 
 @section('content')
 
-  <div class="row">
-      <div class="col-md-3">
-            <div class="panel panel-primary">
-	            <div class="panel-heading">
-		              <h3 class="panel-title">Ações</h3>
-			              </div>
-				              <div class="panel-body">
-					                <a href="/editoras">
-							            <span class="glyphicon glyphicon-th-list"> Editoras</span>
-								              </a>
-									              </div>
-										            </div>
-											        </div>
-												    <div class="col-md-6">
-												    @if(count($errors) > 0)
-												            <div class="alert alert-danger">
-												              <ul>
-												                @foreach ($errors->all() as $message)												    +              <li>{{ $message }}</li>
-												                @endforeach
-												              </ul>
-												            </div>
-												          @endif
-												          <form action="/editoras" method="POST">
-													          <div class="form-group">
-														            <label for="nome">Nome</label>
-															              <input type="text" class="form-control" name="nome" id="nome" />
-																              </div>
-																	              <div class="form-group">
-																		                <label for="email">Email</label>
-																				          <input type="text" class="form-control" name="email" id="email" />
-																					          </div>
-																						          <button class="btn btn-primary">Salvar</button>
-																							        </form>
-																								    </div>
-																								      </div>
-
+<div class="row">
+     <div class="col-md-3">
+     	  <div class="panel panel-primary">
+	       <div class="panel-heading">
+	       	    <h3 class="panel-title">Ações</h3>
+	       </div>
+	       <div class="panel-body">
+	       	    <a href="/editoras">
+		       <span class="glyphicon glyphicon-th-list"> Editoras</span>
+		    </a>
+	       </div>
+	  </div>
+     </div>
+     <div class="col-md-6">
+     	  @if(count($errors) > 0)
+	  <div class="alert alert-danger">
+	       <ul>
+	       @foreach ($errors->all() as $message)
+	       <li>{{ $message }}</li>
+	       @endforeach
+	       </ul>
+	  </div>
+	  @endif
+	  <form action="/editoras" method="POST">
+	  	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	  	<div class="form-group">
+		     <label for="nome">Nome</label>
+		     <input type="text" class="form-control" name="nome" id="nome"/>
+		</div>
+		<div class="form-group">
+		     <label for="email">Email</label>
+		     <input type="text" class="form-control" name="email" id="email"/>
+		</div>
+		<button class="btn btn-primary">Salvar</button>
+	  </form>
+     </div>
+</div>
 @endsection
